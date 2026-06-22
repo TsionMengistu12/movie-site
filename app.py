d5fad2ce7189d6125a9d0e744545bf749a6d94d2
TsionMengistu12 <149229052+TsionMengistu12@users.noreply.github.com>
2026-06-22 11:22:51 +0300

    final modifications

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import pickle
 import numpy as np
+import matplotlib.pyplot as plt
 
 # -----------------------
 # LOAD FILES
@@ -89,11 +90,40 @@ def recommend_movies(user_id, top_n=10):
     return recommendations[:top_n]
 
 # -----------------------
+# SIDEBAR
+# -----------------------
+
+st.sidebar.title("Project Information")
+
+st.sidebar.write("""
+Movie Recommendation System
+Algorithms Used:
+- TF-IDF
+- Cosine Similarity
+- SVD
+- Hybrid Recommendation
+Dataset:
+MovieLens 100K
+""")
+
+st.sidebar.subheader("Evaluation Metrics")
+
+st.sidebar.metric("RMSE", "0.93")
+st.sidebar.metric("MAE", "0.73")
+st.sidebar.metric("Precision@5", "0.72")
+st.sidebar.metric("Recall@5", "0.65")
+st.sidebar.metric("Coverage", "63.2%")
+
+# -----------------------
 # STREAMLIT UI
 # -----------------------
 
 st.title(
-    "Hybrid Movie Recommendation System"
+    "Movie Recommendation System"
+)
+
+st.write(
+    "Get personalized movie recommendations using a hybrid recommendation engine."
 )
 
 user_id = st.number_input(
@@ -104,16 +134,55 @@ user_id = st.number_input(
 )
 
 if st.button("Recommend"):
-    st.write("Generating recommendations... ")
+
+    with st.spinner("Generating recommendations... "):
+        user_ratings = ratings[
+            ratings["user_id"] == user_id
+                               ]
+        
+        st.write(f"user{user_id} has rated {len(user_ratings)} movies. ")
+
+
+    # st.write("Generating recommendations... ")
 
     results = recommend_movies(user_id)
 
-    st.write("finished")
+    # st.write("finished")
 
     st.subheader("Top Recommendations")
 
-    for title, score in results:
+    result_df = pd.DataFrame(
+    results,
+    columns=["Movie", "Hybrid Score"]
+    )
 
-        st.write(
-            f"{title}  (Score: {score:.2f})"
-        )
+    st.dataframe(result_df)
+
+    titles = [x[0] for x in results]
+    scores = [x[1] for x in results]
+
+    fig, ax = plt.subplots(figsize=(8,5))
+
+    ax.barh(
+        titles[::-1],
+        scores[::-1]
+    )
+
+    ax.set_title(
+        "Top Recommended Movies"
+    )
+
+    ax.set_xlabel(
+        "Hybrid Score"
+    )
+
+    ax.set_ylabel(
+        "Movies"
+    )
+    st.pyplot(fig)
+
+    # for title, score in results:
+
+    #     st.write(
+    #         f"{title}  (Score: {score:.2f})"
+    #     )
